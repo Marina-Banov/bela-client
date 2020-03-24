c5df7e2fc3b22f0bc21854df71483e40ef64e124
Marina-Banov <m.banov7@gmail.com>
2020-03-24 23:08:36 +0100

    cards graphics

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -9,11 +9,17 @@ import { NavigationService } from '../services/navigation.service';
 })
 export class MainComponent implements OnInit {
 
+  hand = [];
+
   constructor(protected navigationService: NavigationService,
               protected socketsService: SocketsService) { }
 
   ngOnInit() {
+    this.socketsService.username = this.navigationService.username;
     this.socketsService.emit('new user', this.navigationService.username);
+    this.socketsService.event.subscribe( data => {
+      this.hand = data;
+    });
   }
 
 }
--- src/app/services/sockets.service.ts
+++ src/app/services/sockets.service.ts
@@ -1,16 +1,23 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import * as io from 'socket.io-client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketsService {
+
   private socket: any;
+  public event = new EventEmitter();
+  public username: string;
 
   constructor() {
     this.socket = io('http://localhost:3000');
 
-    this.socket.on('', () => { });
+    this.socket.on('hand', (data) => {
+      if (data.username === this.username) {
+        this.event.emit(data.hand);
+      }
+    });
   }
 
   public emit(eventName: string, data: any) {
